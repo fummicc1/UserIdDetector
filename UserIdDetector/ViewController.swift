abf10012ac3f1bd88b10176ae47e5f09de519b41
fummicc1 <fumiya989@gmail.com>
2019-02-16 20:41:04 +0900

    Initial Commit

---
+++ UserIdDetector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserIdDetector
+//
+//  Created by Fumiya Tanaka on 2019/02/16.
+//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
